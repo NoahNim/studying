5c06cd70003eab633e6e47f63de9a645f976549a
noahnim <nmedoff123@gmail.com>
2022-02-15 13:29:45 -0800

    made basic traversal algorithim

--- stucty/linkedlistwarmup.js
+++ stucty/linkedlistwarmup.js
@@ -8,4 +8,20 @@ class Node {
 const a = new Node('A');
 const b = new Node('B');
 const c = new Node('C');
-const d = new Node('D')
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// A -> B -> C -> D -> null;
+
+const printLinkedList = (head) => {
+    let current = head;
+    while (current != null) {
+        console.log(current.val);
+        current = current.next;
+    }
+}
+
+printLinkedList(a);
