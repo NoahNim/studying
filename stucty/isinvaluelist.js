50f81ddbb1a11d48c28b09cf1038aa90b280ad73
noahnim <nmedoff123@gmail.com>
2022-02-17 11:10:18 -0800

    is in value list

---
+++ stucty/isinvaluelist.js
@@ -0,0 +1,19 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const isUnivalueList = (head) => {
+    let current = head;
+    
+    while (current !== null) {
+      if (current.val !== head.val) return false
+      current = current.next;
+    }
+    
+    return true;
+  };
+  
+  
