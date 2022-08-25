ec603c56a61db5adea6937ba1bf4539661425a4b
noahnim <nmedoff123@gmail.com>
2022-08-25 13:57:01 -0700

    bottom right value refactor

--- stucty/bottomrightvalue.py
+++ stucty/bottomrightvalue.py
@@ -11,46 +11,63 @@
 from collections import deque
 
 def bottom_right_value(root):
-  bottom = root.val
+  queue = deque([root])
+  bot = root.val
   
-  if root.right or root.left:
-    stack_right = [root.right]
-    stack_left = [root.left]
-    bottom_right = root.right
-    bottom_left = root.left
-    bottom_r_count = 0
-    bottom_l_count = 0
-  
-    while stack_right:
-      node = stack_right.pop()
-
-      if node.right:
-        bottom_right = node.right.val
-        stack_right.append(node.right)
-        bottom_r_count += 1
-      if node.left:
-        stack_right.append(node.left)
-        bottom_right = node.left.val
-        bottom_r_count += 1
-      
-    while stack_left:
-      node = stack_left.pop()
-
-      if node.right:
-        bottom_left = node.right.val
-        stack_left.append(node.right)
-        bottom_l_count += 1
-      if node.left:
-        stack_left.append(node.left)
-      
-    print(bottom_l_count)
-    print(bottom_r_count)
-
-    if bottom_r_count > bottom_l_count:
-      bottom = bottom_right
-    if bottom_l_count > bottom_r_count:
-      bottom = bottom_left
-    if bottom_l_count == bottom_r_count:
-      bottom = bottom_right
+  while queue:
+    current  = queue.popleft()
+   
+    if current.left is None:
+      bot = current.val
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+   
+  return bot
     
-  return bottom
+
+# def bottom_right_value(root):
+#   bottom = root.val
+  
+#   if root.right or root.left:
+#     stack_right = [root.right]
+#     stack_left = [root.left]
+#     bottom_right = root.right
+#     bottom_left = root.left
+#     bottom_r_count = 0
+#     bottom_l_count = 0
+  
+#     while stack_right:
+#       node = stack_right.pop()
+
+#       if node.right:
+#         bottom_right = node.right.val
+#         stack_right.append(node.right)
+#         bottom_r_count += 1
+#       if node.left:
+#         stack_right.append(node.left)
+#         bottom_right = node.left.val
+#         bottom_r_count += 1
+      
+#     while stack_left:
+#       node = stack_left.pop()
+
+#       if node.right:
+#         bottom_left = node.right.val
+#         stack_left.append(node.right)
+#         bottom_l_count += 1
+#       if node.left:
+#         stack_left.append(node.left)
+      
+#     print(bottom_l_count)
+#     print(bottom_r_count)
+
+#     if bottom_r_count > bottom_l_count:
+#       bottom = bottom_right
+#     if bottom_l_count > bottom_r_count:
+#       bottom = bottom_left
+#     if bottom_l_count == bottom_r_count:
+#       bottom = bottom_right
+    
+#   return bottom
